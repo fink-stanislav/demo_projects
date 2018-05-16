a7c4777ddb88952f4db44242284cee5c391b66ae
Stanislau Fink <ravenhollm@gmail.com>
2018-05-16 19:12:16 +0300

    adaptation to openshift

---
+++ wsgi.py
@@ -0,0 +1,26 @@
+
+import web
+from PIL import Image
+import io
+#from dbc.classifier import classifier
+
+urls = (
+   '/classify_dog', 'classify_dog',
+   '/test', 'test'
+)
+
+class test:
+    def GET(self):
+        return 'test'
+
+class classify_dog:
+    def POST(self):
+        #form = web.input(image = "img")
+        #image = Image.open(io.BytesIO(form.img))
+        #return classifier.interact(image)
+        return 'post'
+
+app = web.application(urls, globals())
+
+if __name__ == "__main__":
+    app.run()
